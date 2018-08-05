fa074c0199c81a7915f3e56778da9750144ea6e5
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2018-08-05 01:06:12 -0500

    finished retheming

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -6,34 +6,108 @@
 let index = 0;
 // listen for button navigation
 function listenForEvent() {
-	$('#contact-details').on('click', (e) => {
+	$('.contact-btn').on('click', (e) => {
 		console.log('button pressed');
 		e.preventDefault();
 		$('#js-area').show();
 		$('#js-area').html(generateContactDetails());
+		// $('body').css({
+		// 	"background": "linear-gradient(0.25turn, rgb(122, 93, 203), rgb(150, 120, 208), rgb(122, 93, 203))"
+		// });
+		$('header').html(`
+			<h1 role="banner" class="banner" align="center">Contact Details</h1>
+			`);
 	});
-	$('#introduction').on('click',(e) => {
+	$('.intro-btn').on('click',(e) => {
 		$('#js-area').html(generateIntroduction());
+		// $('body').css({
+		// 	"background": "linear-gradient(0.25turn, rgb(104, 158, 250), rgb(124, 165, 246), rgb(104, 158, 250))"
+		// });
+		$('header').html(`
+			<h1 role="banner" class="banner" align="center">Introduction</h1>
+			`);
 	});
-	$('#skillset').on('click', (e) => {
+	$('.skills-btn').on('click', (e) => {
 		$('#js-area').html(generateSkillset());
+		// $('body').css({
+		// 	"background": "linear-gradient(0.25turn, rgb(133, 194, 21), rgb(154, 224, 30), rgb(133, 194, 21))"
+		// });
+		$('header').html(`
+			<h1 role="banner" class="banner" align="center">Skillset / Keywords</h1>
+			`);
 	});
-	$('#work-exp').on('click', (e) => {
+	$('.exp-btn').on('click', (e) => {
 		$('#js-area').html(generateWorkExperience());
+		// $('body').css({
+		// 	"background": "linear-gradient(0.25turn, rgb(221, 128, 9), rgb(247, 155, 37), rgb(221, 128, 9))"
+		// });
+		$('header').html(`
+			<h1 role="banner" class="banner" align="center">Work Experience</h1>
+			`);
 	});
-	$('#projects').on('click', (e) => {
+	$('.project-btn').on('click', (e) => {
 		$('#js-area').html(generateProjects());
+		// $('body').css({
+		// 	"background": "linear-gradient(0.25turn, rgb(254, 89, 83), rgb(235, 133, 131), rgb(254, 89, 83))"
+		// });
+		$('header').html(`
+			<h1 role="banner" class="banner" align="center">Projects</h1>
+			`);
 	});
 	$('#enter-btn').on('click', (e)=> {
 		e.preventDefault();
-		$('.enter-btn').hide();
-		$('.banner').hide();
-		$('.intro-box').show();
+		$('.intro-div').hide();
+		$('.body-head').show();
+		$('#foot').show();
+		$('#friendly').show();
+		$('.contact-box').show();
 		$('#js-area').show();
 		$('footer').show();
-		$('nav').css({
-			"display": "flex",
-			"justify-content": "space-around",
+		$('.nav').show();
+		$('#introduction').hide();
+		$('#skillset').hide();
+		$('#work-exp').hide();
+		$('#contact-details').hide();
+		$('#project-btn').hide();
+
+	})
+	$('.btn-div').hover(() => {
+		$('#text-hide').show();
+		$('#introduction').css({
+			"display": "block"
+		});
+		$('#skillset').css({
+			"display": "block"
+		});
+		$('#work-exp').css({
+			"display": "block"
+		});
+		$('#contact-details').css({
+			"display": "block"
+		});
+		$('#project-btn').css({
+			"display": "block"
+		});
+	})
+	.mouseout(() => {
+
+		$('#introduction').css({
+			"display": "none"
+		});
+		$('#skillset').css({
+			"display": "none"
+		});
+		$('#work-exp').css({
+			"display": "none"
+		});
+		$('#contact-details').css({
+			"display": "none"
+		});
+		$('#project-btn').css({
+			"display": "none"
+		});
+		$('#text-hide').css({
+			"display": "none"
 		});
 	})
 }
--- public/javascript/resume.js
+++ public/javascript/resume.js
@@ -1,20 +1,21 @@
 function generateWorkExperience() {
-	return `<section role="region">
-				<div class="work-exp" class="well">
+	return `
+			<section role="region">
+				<div class="work-exp">
 					<h2 class="header">Work Experience</h2>
 					<ul class="info">
-						<li>Thinkful Student</li>
+						<li class="header">Thinkful Student</li>
 							<ul>
 								<li>"Web Development Fundamentals;" "The Command Line, Git, and Github;" "Asynchronous Client Side Apps;"
 								"Server-side programming with Javascript;" "Learn React;" "Advanced Topics"</li>
-								<li>Started 02/24/2018 - 08/07/2018</li>
+								<li><span class="info">From</span> 02/24/2018 - To 08/07/2018</li>
 							</ul><br><br>
-						<li>HVAC Private Contractor(Self-Employed)</li>
+						<li class="header">HVAC Private Contractor(Self-Employed)</li>
 							<ul>
-							<li>Sales Consultant - Duties include running generated sales leads through <a href="http://www.homeadvisor.com">Homeadvisor.com</a>. I ran all calls generated except when there was an overflow of business. Had a closing rate of around 75%. Sold approximately $150,000 inside of six-month period. In charge of hiring new sales technicians and running the business when the owner was busy with QA. Also in charge of approximately 50% of service calls that involved troubleshooting a broken unit.</li>
-								<li>Started 05/01/17 - 02/24/18</li>
-								<li>Sales/Service Technician - Duties include running generated sales leads through in-house telemarketing. Responded promptly to all calls in a professional manner to either P.M. HVAC equipment or fix broken units. Had a closing rate of around 60% and sold roughly $100,000 in a seven-month period. Tasked with helping on installations from time to time. Also on call during the entire duration of being a contractor.</li>
-								<li>Started 06/01/16 - 05/01/17</li>
+								<li><span class="info">Sales Consultant</span> - To Duties include running generated sales leads through <a href="http://www.homeadvisor.com">Homeadvisor.com</a>. I ran all calls generated except when there was an overflow of business. Had a closing rate of around 75%. Sold approximately $150,000 inside of six-month period. In charge of hiring new sales technicians and running the business when the owner was busy with QA. Also in charge of approximately 50% of service calls that involved troubleshooting a broken unit.</li>
+									<li><span class="info">From</span> 05/01/17 - To 02/24/18</li>
+									<li><span class="info">Sales/Service Technician</span> - Duties include running generated sales leads through in-house telemarketing. Responded promptly to all calls in a professional manner to either P.M. HVAC equipment or fix broken units. Had a closing rate of around 60% and sold roughly $100,000 in a seven-month period. Tasked with helping on installations from time to time. Also on call during the entire duration of being a contractor.</li>
+									<li><span class="info">From</span> 06/01/16 - To 05/01/17</li>
 							</ul>
 					</ul>
 				</div>
@@ -22,18 +23,22 @@ function generateWorkExperience() {
 } 
 function generateContactDetails() {
 	return `
-		<div class="intro-box" hidden>
-			<img id="headshot" src="../images/headshot.jpg" alt="Jim Tisdale" />
-			<div class="contact-details">
-				<div class="address">
-					<p class="header">Jim Tisdale</p>
-					<p class="info">758 Willow Rd.</p>
-					<p class="info">Slocomb, Al 36375</p>
+		<div class="contact-box">
+			<div class="img-banner">
+				<div class="img">
+					<img id="headshot" src="../images/headshot.jpg" alt="Jim Tisdale" />
 				</div>
-				<div class="phone">
-					<p class="info">P: (334)500-0066</p>
-					<p class="info">E: JCTisdale1988@gmail.com</p>
-					<p class="info">DOB: 10/07/1988</p>
+				<div class="contact-details">
+					<div class="address">
+						<p class="header">Jim Tisdale</p>
+						<p class="info">758 Willow Rd.</p>
+						<p class="info">Slocomb, Al 36375</p>
+					</div>
+					<div class="phone">
+						<p class="info">10/07/1988</p>
+						<p class="info">(334)500-0066</p>
+						<p class="info">JCTisdale1988@gmail.com</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -42,56 +47,63 @@ function generateContactDetails() {
 function generateIntroduction() {
 	return `
 		<div id="bio" class="well">
-			<h2 class="header">Introduction</h2><br>
-				<p class="info">  I am a junior developer with a passion for learning. I have been learning on my
-				own for the past couple of years. In Febuary of this year, I decided to make the change into
-				my new career! I fell in love with all the amazing things you can do with Javascript and how there is so much
-				opportunity in this field. I am very excited to get into the nuances of Javascript
-				and even trying out Tensorflow.js.</p><br><br>
-				<p class="info1 info">While I was studying at Thinkful, I had the chance to learn from real professionals who are already in this
-				industry, and have been able to learn the core essentials of writing good modular
-				code.  I cant wait to see how future technologies will shape my career!</p>
+			<p class="info">  
+				When I imagined what a career in tech would be like,
+				I had no indication it was going to be such a journey.<br><br>
+				The developer career track is fraught with roadblocks and headaches,
+				but have persevered by digging deep and achieving my goals.<br><br> 
+				I believe my passion for web development is going to help me grow from a junior developer
+				into a project manager and ultimately an entrepreneur. 
+				I want to be able to transform my ideas into products from which people will truly benefit.<br><br>
+				People say that persistence and hard work is the key to success.
+				What they don't tell you is that you can't just get there on persistence and hard work alone.
+				You need ambition and the will to surpass your obstacles,
+				so that there is no ceiling to your achievements.<br><br>
+				Please feel free share your success stories, or better yet, 
+				share some rock-solid tips for being a better developer. 
+			</p>
 		</div>
 	`
 }
 function generateProjects() {
 	return `
-		<div class="projects">
-			<div class="pic-container">
-				<div class="overlay">
-					<p>text here</p>
+			<div class="projects">
+				<div class="pic-container">
+					<div class="overlay white sm-text">
+						<p class="header o-header md-text">Flow-State</p>
+						<p class="info xsm-text">A better way to work</p>
+						<p class="header sm-text o-footer">Created with: 
+							<span class="info xsm-text">HTML5, CSS3, Javascript ES6, Asynchronous AJAX calls, API Integration,
+							Node.js, Mongoose, MongoDb, NodeFS</span>
+						</p>
+					</div>
+					<div class="flow">
+						<a href="https://flow-state.herokuapp.com"><img id="flow-pic" class="thumbnail" src="images/flow-example.jpg"></a>	
+					</div>
 				</div>
-				<div class="flow">
-					<a href="https://flow-state.herokuapp.com"><img id="flow-pic" class="thumbnail" src="images/flow-example.jpg"></a>	
+				<div class="pic-container">
+					<div class="overlay white darker sm-text">
+						<p class="header o-header md-text">Sing'N'Go</p>
+						<p class="info xsm-text" align="center">Get your favorite artist's lyrics and showtimes!</p>
+						<p class="info sm-text o-footer">Created with: HTML5, CSS3, Javascript ES6, Asynchronous AJAX calls, API Integration</p>
+					</div>
+					<div class="sing">
+						<a href="https://blubagoo.github.io/singngo/"><img class="thumbnail" id="sing-pic"src="images/singbg.jpg"></a>
+					</div>
 				</div>
+				<div class="pic-container">
+					<div class="overlay white darker sm-text">
+						<p class="header o-header md-text">Think you're a gamer?</p>
+						<p class="info xsm-text">Test your gamer knowledge</p>
+						<p class="info sm-text o-footer">Created with: HTML5, CSS3, Javascript</p>
+					</div>
+					<div class="quiz">
+						<a href="https://repl.it/@JimTisdale/Quiz-App"><img class="thumbnail" id="quiz-pic" src="images/quizbg.jpg"></a>
+					</div>
+				</div>	
 			</div>
-			<div class="pic-container">
-				<div class="overlay">
-				</div>
-				<div class="sing">
-					<a href="https://blubagoo.github.io/singngo/"><img class="thumbnail" id="sing-pic"src="images/singbg.jpg"></a>
-				</div>
-			</div>
-			<div class="pic-container">
-				<div class="overlay">
-				</div>
-				<div class="quiz">
-					<a href="https://repl.it/@JimTisdale/Quiz-App"><img class="thumbnail" id="quiz-pic" src="images/quizbg.jpg"></a>
-				</div>
-			</div>	
-		</div>
 	`
 }
-
-				// <p class="cap-text">Flow-State<br><span class="cap-def">A better way to work</span></p>
-				// <p class="tech">Technolgies used: HTML5, CSS3, Javascript ES6, Asynchronous AJAX calls, API Integration,
-				// 	Node.js, Mongoose, MongoDb, NodeFS</p>
-				// <p class="cap-text">Think you're a gamer?<br><span class="cap-def">Test your gamer knowledge</span></p>
-				// <p class="tech">Technolgies used: HTML5, CSS3, Javascript</p>
-				// <p class="cap-text">Sing'N'Go<br><span class="cap-def">Get your favorite artist's lyrics and showtimes!</span></p>
-				// <p class="tech">Technolgies used: HTML5, CSS3, Javascript ES6, Asynchronous AJAX calls, API Integration</p>
-
-
 function generateSkillset() {
 	return `
 		<div class="skillset">
